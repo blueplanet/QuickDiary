dd3e4ea654c84296c74f1cf462f2b53060f6b3d2
blueplanet <erguolinge@gmail.com>
2012-06-09 11:13:23 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuickDiary::Application.config.secret_token = '5e174edfdb01abf0c23321222d40a42234a83554818d42286f9cdfb8e2e52bdd54823ff3fde99f2c00e87fe4d1885a60e290b56f1ce08f33887a9203a015c432'
